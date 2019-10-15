5900db820c58531ef9b825036da35f80a7faf4e1
j040n3t0 <joaojose.ti@gmail.com>
2019-10-14 22:09:50 -0300

    Update Files

--- json_io.py
+++ json_io.py
@@ -55,8 +55,8 @@ def worker():
 				"range":{\
 					"@timestamp":{\
 						"format":"strict_date_optional_time",\
-						"gte":"2019-10-09T20:55:00.000Z",\
-						"lte":"2019-10-09T23:59:59.999Z"\
+						"gte":"2019-10-14T20:55:00.000Z",\
+						"lte":"2019-10-14T23:59:59.999Z"\
 					}\
 				}\
 			}],"filter":[{\
@@ -69,17 +69,48 @@ def worker():
 	}'
 
 	data2 = '{"query": {"bool": {"must": [{"range": {"@timestamp": {"gte": "now-15s", "lte": "now"}}},{"query_string": {"query": "docker.memory.usage.pct: *"}}]}}}'
-	response2 = requests.post('http://%s:9200/metricbeat-2019.10.14/_search' % ip_elasticsearch, headers=headers, data=data2)
-	response2_json = response2.json()
-	print "\n\nJSON 2"
-	#print response2_json
-	print response2_json['hits']['hits']
-	print "#########################\n"
+	try:
+		response2 = requests.post('http://%s:9200/metricbeat-2019.10.15/_search' % ip_elasticsearch, headers=headers, data=data2)
+		response2_json = response2.json()
+		print "\n\nJSON 2"
+		#print response2_json
+		search_size = response2_json['hits']['total']['value']
+		print search_size
+		print "#########################\n"
+	except:
+		print "Nada feito! hehe'"
 
+	data2 = '{"size": %s, "query": {"bool": {"must": [{"range": {"@timestamp": {"gte": "now-15s", "lte": "now"}}},{"query_string": {"query": "docker.memory.usage.pct: *"}}]}}}' % search_size
+	try:
+		response2 = requests.post('http://%s:9200/metricbeat-2019.10.15/_search' % ip_elasticsearch, headers=headers, data=data2)
+		response2_json = response2.json()
+		print "\n\nJSON 2"
+		#print response2_json
+		#print response2_json['hits']['hits'][0]["_source"]["container"]["name"]
+		#print response2_json['hits']['hits'][0]["_source"]["docker"]["memory"]["usage"]["pct"]
+		memory_list = response2_json['hits']['hits']
+		print "#########################\n"
+	except:
+		print "Nada feito! hehe'"
 
-	response = requests.post('http://%s:9200/metricbeat-2019.10.09/_search' % ip_elasticsearch, headers=headers, data=data)
+	containers_list_name = []
+	containers_memory_usage = []
+	for i in memory_list:
+		containers_list_name.append(i["_source"]["container"]["name"])
+		convert_pct = float(i["_source"]["docker"]["memory"]["usage"]["pct"]) * 100
+		print i["_source"]["docker"]["memory"]["usage"]["pct"]
+		print convert_pct
+		containers_memory_usage.append("%.2f" % convert_pct)
+
+	#for i in containers_memory_usage:
+	#	print i
+	#for i in containers_list_name:
+	#	print i
+
+	response = requests.post('http://%s:9200/metricbeat-2019.10.14/_search' % ip_elasticsearch, headers=headers, data=data)
 
 	response_json = response.json()
+	#print response_json
 	buckets = response_json['aggregations']['2']['buckets']
 	containers_list = []
 	containers_docs = []
@@ -99,7 +130,9 @@ def worker():
 		'name': render_template('response.html', name=name),
 		'message': render_template('response.html', message=message),
 		'containers_list': render_template('response.html', containers_list=containers_list),
-		'containers_docs': render_template('response.html', containers_docs=containers_docs)
+		'containers_docs': render_template('response.html', containers_docs=containers_docs),
+		'containers_list_name': render_template('response.html', containers_list_name=containers_list_name),
+		'containers_memory_usage': render_template('response.html', containers_memory_usage=containers_memory_usage)
 	})
 
 if __name__ == "__main__":
